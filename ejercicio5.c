af84388dbe016b107136382b6ae9bb4f89cf5053
Mihai <mihaiblidaru14@gmail.com>
2017-11-06 11:26:13 +0100

    Cambio a Qs medio

--- ejercicio5.c
+++ ejercicio5.c
@@ -65,7 +65,7 @@ int main(int argc, char** argv){
   }
 
   /* calculamos los tiempos */
-    ret = genera_tiempos_ordenacion(MergeSort, nombre,num_min, num_max,incr, n_perms);
+    ret = genera_tiempos_ordenacion(QuickSort, nombre,num_min, num_max,incr, n_perms);
   if (ret == ERR) { /* ERR_TIME debera ser un numero negativo */
     printf("Error en la funcion Time_Ordena\n");
     exit(-1);
